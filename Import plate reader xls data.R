24d174b104941726f7e7d1c4a4247483da9bd25b
Alex <aleks.delta2013@gmail.com>
2021-11-22 17:54:31 +0100

    added time difference calculation

--- Import plate reader xls data.R
+++ Import plate reader xls data.R
@@ -2,9 +2,10 @@ library("readxl")
 library("writexl")
 library(rio)
 library(xlsx)
+library(dplyr)
 
-files_in_folder= 24 #здесь кол-во файлов
-count_points=4 #кол-во измерений
+files_in_folder= 20 #здесь кол-во файлов
+count_points=5 #кол-во измерений
 folder_path= "E:/input_data" #папка входных данных
 files_in_folder=files_in_folder/count_points
 
@@ -30,6 +31,15 @@ for (i in 1:files_in_folder) {
     filename= xlsfiles[N]
     datapath= file.path(folder_path,paste(filename,".xls", sep=""))
     dataset <- read_excel(datapath, sheet = sheet)
+    Date_modified=file.mtime(datapath)
+    if (N==1) {
+      Date_modified_0=file.mtime(datapath)
+      dataset[1, 8] = 0
+    } else {
+      as.numeric(Date_modified)-as.numeric(Date_modified_0)
+    }
+    dataset[1, 7] = toString(Date_modified, format="%Y-%m-%d %H:%M:%S")
+    
     File=rbind(File, dataset)
   }
   print(i)
@@ -40,6 +50,11 @@ for (i in 1:files_in_folder) {
   n=n+count_points
   k=k+count_points
 }
+
+
+
+
+
 
 
 
